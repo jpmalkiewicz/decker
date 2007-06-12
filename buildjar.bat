393aa2c7ed1524c6c8f0fc093d845c14f4720aff
lexaeus <lexaeus@dd657f7f-a132-0410-9dd7-a7c2ace12f42>
2007-06-12 11:04:04 +0000

    Fixed buildjar script

--- buildjar.bat
+++ buildjar.bat
@@ -1,3 +1,3 @@
 @echo off
-jar cvfm JDecker.jar manifest decker rulesets
+jar cvfm JDecker.jar manifest decker/*.class decker/model/*.class decker/util/*.class decker/view/*.class rulesets/*.txt rulesets/*.bmp rulesets/decker/*.txt rulesets/decker/artwork/*.bmp rulesets/decker/artwork/character_image/*.bmp rulesets/decker/artwork/home/*.bmp 
 pause
