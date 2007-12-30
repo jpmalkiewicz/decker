46b49fdce0b1669d65335813d19e1daa356d51eb
chabbrik <chabbrik@dd657f7f-a132-0410-9dd7-a7c2ace12f42>
2007-12-30 14:51:11 +0000

    Removed initial println as not making sense. Formatted indentation. Commented out streaming to Error.java Refactored some variable names to make them easier to understand.

--- decker/Decker.java
+++ decker/Decker.java
@@ -3,54 +3,57 @@ import decker.model.*;
 import decker.view.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.*;
+import decker.view.SplashScreen;
 
-
-
-/** all this class does is start the program */
+/** 
+ *  Main class, that is used to start the game.   
+ */
 public class Decker
 {
 	public static void main(String[] args) {
-System.out.println("there seems to be an 'else' clause missing in Node.cpp:411");
-System.out.println();
-System.out.println("the game should display a placeholder image when an image is missing, instead of just erroring out");
-System.out.println("change : more than one ICE can trace the player simultaneously");
-System.out.println("FunctionCall.execute_filelist() needs to take mods into account");
-System.out.println("when a script doesn't have any accepted locales, say so in a message popup");
-System.out.println("is Global.setCurrentRuleset() safe? what happens if the ruleset changes during a trigger check?");
-System.out.println("AbstractView.getImage() : need to put localized images back in, no support for mod images yet, loaded images aren't stored for later reuse yet");
-		System.out.println("Errors are written to ERROR.JAVA for testing purposes");
-System.out.println();
-		try {
-			System.setErr(new PrintStream(new FileOutputStream("ERROR.JAVA",false)));
-		} catch (FileNotFoundException ex) {
-			System.exit(1);
-		}
-		// load the rulesets
-		decker.view.SplashScreen ss = new decker.view.SplashScreen();
+//		
+//		try {
+//			System.setErr(new PrintStream(new FileOutputStream("ERROR.JAVA", false)));
+//		} catch (FileNotFoundException ex) {
+//			System.exit(1);
+//		}
+		
+		/* loading the rulesets */
+		SplashScreen ss = new SplashScreen();
 		Global.setDisplayedComponent(ss);
 		Global.initializeDataModel();
 		Global.loadRulesets();
 		// center the game window on the screen by default
-		Global.getEngineData().add("display_center_x").set(ss.getToolkit().getScreenSize().width/2);
-		Global.getEngineData().add("display_center_y").set(ss.getToolkit().getScreenSize().height/2);
-Global.initializeRulesets();
-Global.setCurrentRuleset(decker.model.Global.ruleset[0]);
+		Global.getEngineData().add("display_center_x").set(
+				ss.getToolkit().getScreenSize().width / 2);
+		Global.getEngineData().add("display_center_y").set(
+				ss.getToolkit().getScreenSize().height / 2);
+		Global.initializeRulesets();
+		Global.setCurrentRuleset(decker.model.Global.ruleset[0]);
 		ss.setVisible(false);
-Frame f = new Frame();
-Global.setDisplayedComponent(f);
-f.setLayout(new BorderLayout());
-f.add(decker.model.Global.getViewWrapper());
-new decker.view.FPSThread(decker.model.Global.getViewWrapper());
-f.setBounds(Global.getEngineData().get("display_center_x").integer()-100/2, Global.getEngineData().get("display_center_y").integer()-70/2, 100, 70);
-f.setVisible(true);
-f.addWindowListener(	new WindowAdapter() { public void windowClosing (WindowEvent event) { System.exit(0); }} );
-f.addFocusListener( new FocusAdapter() { public void focusGained(FocusEvent e) { Global.getViewWrapper().requestFocus(); }} );
-Global.getViewWrapper().setView(new DefaultView());
-AbstractView.reloadArtwork(true);
-Global.getDisplayedScreen().set(Global.getCurrentRuleset().data.get("initial_screen"));
-Global.getViewWrapper().repaint();
-Global.getViewWrapper().requestFocus();
+		Frame frame = new Frame();
+		Global.setDisplayedComponent(frame);
+		frame.setLayout(new BorderLayout());
+		frame.add(decker.model.Global.getViewWrapper());
+		new decker.view.FPSThread(decker.model.Global.getViewWrapper());
+		frame.setBounds(Global.getEngineData().get("display_center_x").integer() - 100 / 2, Global
+				.getEngineData().get("display_center_y").integer() - 70 / 2, 100, 70);
+		frame.setVisible(true);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent event) {
+				System.exit(0);
+			}
+		});
+		frame.addFocusListener(new FocusAdapter() {
+			public void focusGained(FocusEvent e) {
+				Global.getViewWrapper().requestFocus();
+			}
+		});
+		Global.getViewWrapper().setView(new DefaultView());
+		AbstractView.reloadArtwork(true);
+		Global.getDisplayedScreen().set(Global.getCurrentRuleset().data.get("initial_screen"));
+		Global.getViewWrapper().repaint();
+		Global.getViewWrapper().requestFocus();
 	}
 
 
