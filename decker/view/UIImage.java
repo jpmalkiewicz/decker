93660646dd01a5d3a223a7abc413b7427e7d38c8
Zyphrus <Zyphrus@dd657f7f-a132-0410-9dd7-a7c2ace12f42>
2008-08-07 14:33:53 +0000

    you can now play a whole game of battleships

--- decker/view/UIImage.java
+++ decker/view/UIImage.java
@@ -12,38 +12,7 @@ class UIImage extends DisplayedComponent
 
 	UIImage (final Value _component, final DisplayedComponent _parent, final DisplayedComponent current_clip_source) {
 		super(_component, _parent);
-		if (!_component.equalsConstant("UNDEFINED")) {
-			boolean load_normal_image = true;
-			String image_name = null;
-			int angle = 0;
-			if (_component.type()==Value.STRUCTURE && _component.get("structure_type").equals("IMAGE")) {
-				if (!_component.get("image").equalsConstant("UNDEFINED")) {
-					image_name = _component.get("image").toString();
-					// if the image has been turned, try to load the turned version
-					final Value v = _component.get("angle");
-					if (v != null) {
-						final int t = v.type();
-						if (t == Value.INTEGER || t == Value.REAL) {
-							angle = (t == Value.INTEGER) ? v.integer() : (int)v.real();
-							// set the angle to 0, 90, 180 or 270. -45 is rounded to -90, +45 is rounded to +90
-							angle = ( (((angle%360)+((angle<0)?404:45))%360) / 90 ) * 90;
-						}
-					}
-				}
-			}
-			else {
-				image_name = _component.toString();
-			}
-
-			// fetch the image
-			if (image_name != null && load_normal_image) {
-				image = (angle == 0) ? AbstractView.getImage(image_name) : AbstractView.getTurnedImage(image_name, angle);
-				if (image == null) {
-					final Value image_value = (_component.type()==Value.STRUCTURE&&_component.get("structure_type").equals("IMAGE")) ? _component.get("image") : _component;
-					System.out.println("UIImage : undefined image "+image_value+" ("+image_value.typeName()+")");
-				}
-			}
-		}
+		fetchImage();
 		if (_component.type() == Value.STRUCTURE)
 			_component.structure().addValueListener(this);
 		update(0, current_clip_source);
@@ -88,24 +57,45 @@ class UIImage extends DisplayedComponent
 			final int old_width = w, old_height = h;
 			final boolean old_rtpw = relative_to_parent_width, old_rtph = relative_to_parent_height;
 			final Image old_image = image;
-			final String image_name = variable_name.equals("image") ? new_value.toString() : component.get("image").toString();
-			final Value v_angle = component.get("angle");
-			final int angle = (v_angle != null &&( v_angle.type() == Value.INTEGER || v_angle.type() == Value.REAL )) ? (int) v_angle.real() : 0;
-			image = AbstractView.getTurnedImage(image_name, angle);
-			if ((v=component.get("width")) == null || v.equalsConstant("UNDEFINED")) {
-				w = (image != null) ? image.getWidth(null) : 0;
-				relative_to_parent_width = (v=component.get("x")) != null &&( v.equalsConstant("CENTER") || v.equalsConstant("RIGHT") ||( (k=getPercentageValue(v.toString())) != Integer.MIN_VALUE && k != 0 ));
-			}
-			if ((v=component.get("height")) == null || v.equalsConstant("UNDEFINED")) {
-				h = (image != null) ? image.getHeight(null) : 0;
-				relative_to_parent_height = (v=component.get("y")) != null &&( v.equalsConstant("CENTER") || v.equalsConstant("BOTTOM") ||( (k=getPercentageValue(v.toString())) != Integer.MIN_VALUE && k != 0 ));
-			}
+			fetchImage();
 			if (w != old_width || h != old_height || old_rtpw != relative_to_parent_width || old_rtph != relative_to_parent_height) {
 				eventSizeChanged(getCurrentClipSource(), old_width, old_height, old_rtpw, old_rtph);
 			}
 		}
 		else {
 			super.eventValueChanged(variable_name, container, old_value, new_value);
+		}
+	}
+
+
+
+
+	private void fetchImage () {
+		// fetch the image
+		Value v;
+		String image_name;
+		int angle = 0;
+		if (component.type() != Value.STRUCTURE || component.get("image") == null) {
+			image_name = component.toString();
+		}
+		else {
+			image_name = component.get("image").toString();
+			angle = ((v=component.get("angle")) != null &&( v.type() == Value.INTEGER || v.type() == Value.REAL )) ? (int) v.real() : 0;
+		}
+		image = (angle!=0) ? AbstractView.getTurnedImage(image_name, angle) : AbstractView.getImage(image_name);
+		// print an error if the image is missing
+		if (image == null && !image_name.equals("UNDEFINED")) {
+			System.out.println("UIImage : undefined image "+image_name+" ("+((component.type() != Value.STRUCTURE || component.get("image") == null)?component:component.get("image")).typeName()+")");
+		}
+		// adjust the component bounds
+		int k;
+		if ((v=component.get("width")) == null || v.equalsConstant("UNDEFINED")) {
+			w = (image != null) ? image.getWidth(null) : 0;
+			relative_to_parent_width = (v=component.get("x")) != null &&( v.equalsConstant("CENTER") || v.equalsConstant("RIGHT") ||( (k=getPercentageValue(v.toString())) != Integer.MIN_VALUE && k != 0 ));
+		}
+		if ((v=component.get("height")) == null || v.equalsConstant("UNDEFINED")) {
+			h = (image != null) ? image.getHeight(null) : 0;
+			relative_to_parent_height = (v=component.get("y")) != null &&( v.equalsConstant("CENTER") || v.equalsConstant("BOTTOM") ||( (k=getPercentageValue(v.toString())) != Integer.MIN_VALUE && k != 0 ));
 		}
 	}
 
